2133d902c5f81a4808e4c8eef8f8b79267499c03
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-25 21:58:30 +0300

    Finished Footer rstructure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,39 @@
+import footerImage from "./../../img/restaurantfood.jpg"
+
 export function Footer() {
     return (
         <footer>
-            <p>Footer</p>
+            <img src={footerImage} alt="food display"/>
+            <section>
+                <h6>doormat navigation</h6>
+                <ul>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>menu</li>
+                    <li>reservations</li>
+                    <li>order online</li>
+                    <li>login</li>
+                </ul>
+            </section>
+            <section>
+                <h6>contact</h6>
+                <ul>
+                    <li>address</li>
+                    <li>phone number</li>
+                    <li>email</li>
+                    <li>reservations</li>
+                    <li>order online</li>
+                    <li>login</li>
+                </ul>
+            </section>
+            <section>
+                <h6>social media links</h6>
+                <ul>
+                    <li>address</li>
+                    <li>phone number</li>
+                    <li>email</li>
+                </ul>
+            </section>
         </footer>
     )
 }
