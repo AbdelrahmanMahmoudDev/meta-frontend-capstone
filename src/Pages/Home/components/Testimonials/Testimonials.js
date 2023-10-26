a4bc99e4bf17e488203d5f59c97b8809437130cd
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-26 23:05:17 +0200

    Finished Homepage testimonials

--- src/Pages/Home/components/Testimonials/Testimonials.js
+++ src/Pages/Home/components/Testimonials/Testimonials.js
@@ -1,9 +1,54 @@
 import styles from "./Testimonials.module.css"
+import {Container} from "./../../../common"
+const data = [
+    {
+        title: "name",
+        rating: "rating",
+        reviewText: "reviewText",
+        image: require("./../../../../img/profile-1.jpg")
+    },
+    {
+        title: "name",
+        rating: "rating",
+        reviewText: "reviewText",
+        image: require("./../../../../img/profile-2.jpg")
+    },
+    {
+        title: "name",
+        rating: "rating",
+        reviewText: "reviewText",
+        image: require("./../../../../img/profile-3.jpg")
+    },
+    {
+        title: "name",
+        rating: "rating",
+        reviewText: "reviewText",
+        image: require("./../../../../img/profile-4.jpg")
+    },
+]
 
 export function Testimonials() {
     return (
-        <div className={styles.testimonials}>
-            <p>Testimonials</p>
-        </div>
+        <section className={styles.testimonials}>
+            <Container>
+                <h1>Testimonials</h1>
+                <section className={styles.cardContainer}>
+                    {data.map((item, index) => (
+                        <article key={index}>
+                            <div className={styles.row}>
+                                <p>{item.rating}</p>
+                            </div>
+                            <div className={styles.row}>
+                                <img src={item.image} alt={item.title}/>
+                                <p>{item.title}</p>
+                            </div>
+                            <div className={styles.row}>
+                                <p>{item.reviewText}</p>
+                            </div>
+                        </article>
+                    ))}
+                </section>
+            </Container>
+        </section>
     )
 }
