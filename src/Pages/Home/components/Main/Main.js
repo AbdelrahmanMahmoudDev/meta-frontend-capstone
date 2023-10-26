68706be47b6e0d2065e3ec1de91354194ff9384d
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-26 13:54:05 +0300

    Removed unused imports

--- src/Pages/Home/components/Main/Main.js
+++ src/Pages/Home/components/Main/Main.js
@@ -3,12 +3,6 @@ import {Hero} from "./../Hero"
 import {Specials} from "./../Specials"
 import {Testimonials} from "./../Testimonials"
 import {About} from "./../About"
-import {Menu} from "../../../Menu"
-import { Reservations } from "../../../Reservations"
-import { OrderOnline } from "../../../OrderOnline"
-import { Login } from "../../../Login"
-
-import {Routes, Route} from "react-router-dom"
 
 export function Main() {
     return (
