5aaa7b90b630f7575029208c6f1163faf8d3aa13
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-27 19:30:25 +0200

    Finished Homepage about section

--- src/Pages/Home/components/About/About.js
+++ src/Pages/Home/components/About/About.js
@@ -1,7 +1,21 @@
+import styles from "./About.module.css"
+import {Container} from "./../../../common"
+
 export function About() {
     return (
-        <div id="about">
-            <p>About</p>
-        </div>
+        <section className={styles.aboutSection}>
+            <Container className="container">
+                <article>
+                    <h1>little lemon</h1>
+                    <h4>chicago</h4>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+                </article>
+                <article>
+                    <img src={require("./../../../../img/restaurant.jpg")} alt="restaurant" />
+                    <img src={require("./../../../../img/restaurant chef B.jpg")} alt="restaurant chef" />
+                </article>
+            </Container>
+        </section>
     )
 }
