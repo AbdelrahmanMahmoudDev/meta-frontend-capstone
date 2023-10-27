a7f8e4917cb43b91d8b6825ebdb1cd3a279b9009
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-27 20:04:41 +0200

    Finished Homepage footer section

--- src/Pages/Home/components/Footer/Footer.js
+++ src/Pages/Home/components/Footer/Footer.js
@@ -1,39 +1,45 @@
 import footerImage from "./../../../../img/restaurantfood.jpg"
 import styles from "./Footer.module.css"
 
+import {Container} from "./../../../common"
+
 export function Footer() {
     return (
-        <footer className={styles.footer}>
-            <img src={footerImage} width="239px" height="347px" alt="food display"/>
+        <footer className={styles.footerSection}>
             <section>
-                <h6>doormat navigation</h6>
-                <ul>
-                    <li>home</li>
-                    <li>about</li>
-                    <li>menu</li>
-                    <li>reservations</li>
-                    <li>order online</li>
-                    <li>login</li>
-                </ul>
-            </section>
-            <section>
-                <h6>contact</h6>
-                <ul>
-                    <li>address</li>
-                    <li>phone number</li>
-                    <li>email</li>
-                    <li>reservations</li>
-                    <li>order online</li>
-                    <li>login</li>
-                </ul>
-            </section>
-            <section>
-                <h6>social media links</h6>
-                <ul>
-                    <li>address</li>
-                    <li>phone number</li>
-                    <li>email</li>
-                </ul>
+                <section>
+                    <img src={footerImage} width="239px" height="347px" alt="food display"/>
+                </section>
+                <section>
+                    <h6>doormat navigation</h6>
+                    <ul>
+                        <li>home</li>
+                        <li>about</li>
+                        <li>menu</li>
+                        <li>reservations</li>
+                        <li>order online</li>
+                        <li>login</li>
+                    </ul>
+                </section>
+                <section>
+                    <h6>contact</h6>
+                    <ul>
+                        <li>address</li>
+                        <li>phone number</li>
+                        <li>email</li>
+                        <li>reservations</li>
+                        <li>order online</li>
+                        <li>login</li>
+                    </ul>
+                </section>
+                <section>
+                    <h6>social media links</h6>
+                    <ul>
+                        <li>address</li>
+                        <li>phone number</li>
+                        <li>email</li>
+                    </ul>
+                </section>
             </section>
         </footer>
     )
